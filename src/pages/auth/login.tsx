4c6e569818ddb600f0c5e7b7399f466450531d60
NanoB0tt <marianofassikoropecki@gmail.com>
2023-09-05 10:32:07 -0300

    added login and register pages

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,5 +1,5 @@
 import { LoginForm } from "./components";
-import FormLayout from "./layout/form-layout";
+import { FormLayout } from "./layout/form-layout";
 
 export function Login() {
   return (
--- src/pages/auth/register.tsx
+++ src/pages/auth/register.tsx
@@ -1,5 +1,5 @@
 import { RegisterForm } from "./components";
-import FormLayout from "./layout/form-layout";
+import { FormLayout } from "./layout/form-layout";
 
 
 export function Register() {
