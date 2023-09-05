9ad9e4f5c39ab987580e82ca1662ee66f7945c41
NanoB0tt <marianofassikoropecki@gmail.com>
2023-09-05 20:35:03 -0300

    removed unused code

--- src/pages/root.tsx
+++ src/pages/root.tsx
@@ -1,6 +1,3 @@
-/* import { Avatar, Box, Flex, Text } from "@chakra-ui/react"; */
-/* import { useAuth } from "../context"; */
-
 import { Container } from "@chakra-ui/react";
 import { Sidebar } from "../components/sidebar";
 import { Outlet } from "react-router-dom";
@@ -11,9 +8,7 @@ export function Root() {
   return (
     <Container p='unset' m='unset' maxW='unset' h='100vh' display='grid' gridTemplateColumns='25rem 1fr'>
       <Sidebar />
-      <div>
-        <Outlet />
-      </div>
+      <Outlet />
     </Container>
   )
 }
