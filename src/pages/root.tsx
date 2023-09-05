db73d8e37dbadd6c437051d1ef50d40fdc93caa0
NanoB0tt <marianofassikoropecki@gmail.com>
2023-09-05 10:33:15 -0300

    added root page

--- src/pages/root.tsx
+++ src/pages/root.tsx
@@ -3,13 +3,17 @@
 
 import { Container } from "@chakra-ui/react";
 import { Sidebar } from "../components/sidebar";
+import { Outlet } from "react-router-dom";
 
 
 export function Root() {
 
   return (
-    <Container p='unset' m='unset' h='100vh'>
+    <Container p='unset' m='unset' maxW='unset' h='100vh' display='grid' gridTemplateColumns='25rem 1fr'>
       <Sidebar />
+      <div>
+        <Outlet />
+      </div>
     </Container>
   )
 }
