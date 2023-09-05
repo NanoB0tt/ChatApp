9f546125216aee4cc644caf543249e3bb6d8c31d
NanoB0tt <marianofassikoropecki@gmail.com>
2023-09-05 20:42:01 -0300

    component takes childrens now

--- src/components/friend-profile.tsx
+++ src/components/friend-profile.tsx
@@ -1,25 +1,27 @@
-import { Avatar, Box, Button, Flex, Text } from "@chakra-ui/react";
+import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 import { User } from "../context/interfaces";
+import { ReactNode } from "react";
 
 interface Props {
   friend: User;
   isHeader?: boolean;
-  isFriendRequest?: boolean
-  sendFriendRequest: (id: string) => Promise<void>
+  children?: ReactNode;
 }
 
-export function FriendProfile({ friend, isHeader, isFriendRequest, sendFriendRequest }: Props) {
+export function FriendProfile({ friend, isHeader, children }: Props) {
   return (
     <Flex alignItems='center' p='2' h={isHeader ? '4.4rem' : '5rem'} border='2px' borderColor='blue' _hover={{
       cursor: "pointer"
     }}>
-      <Avatar src={friend.imagePath} w='3rem' h='3rem' />
-      <Box ml='3'>
-        <Text fontWeight='normal' fontSize='1.3rem'>
-          {friend.userName}
-        </Text>
-      </Box>
-      {isFriendRequest && <Button onClick={() => sendFriendRequest(friend.id)}>Invite</Button>}
+      <Flex alignItems='center'>
+        <Avatar src={friend.imagePath} w='3rem' h='3rem' />
+        <Box ml='3'>
+          <Text fontWeight='normal' fontSize='1.3rem'>
+            {friend.userName}
+          </Text>
+        </Box>
+      </Flex>
+      {children}
     </Flex>
   )
 }
