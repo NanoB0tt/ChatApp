7d2a7dd5de9e5b56e69bd678cfe0f38633427a04
NanoB0tt <marianofassikoropecki@gmail.com>
2023-09-05 10:27:41 -0300

    added search friends component

--- src/components/search-friends.tsx
+++ src/components/search-friends.tsx
@@ -1,16 +1,21 @@
-import { useEffect, useState } from "react";
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
 import { User } from "../context/interfaces";
 import { useAxiosPrivate } from "../hooks/useAxiosPrivate";
-import { Avatar, Box, Button, Flex, Text, Input } from "@chakra-ui/react";
-import { nanoid } from "nanoid";
+import { Box, Input } from "@chakra-ui/react";
+import { FriendProfile } from "./friend-profile";
 
 const SEARCH_FRIENDS_URL = '/api/user/search/friend/';
 const SEND_FRIEND_REQUEST_URL = '/api/user/friend-request/send/'
 
-export function SearchFriends() {
+interface Props {
+  setIsSearching: Dispatch<SetStateAction<boolean>>
+}
+
+export function SearchFriends({ setIsSearching }: Props) {
   const axiosPrivate = useAxiosPrivate();
   const [searchFriends, setSearchFriends] = useState<User[]>();
   const [searchInput, setSearchInput] = useState('');
+  const isFriendRequest = true;
 
 
   useEffect(() => {
@@ -19,8 +24,10 @@ export function SearchFriends() {
         if (searchInput.length >= 3) {
           const response = await axiosPrivate.get(SEARCH_FRIENDS_URL + searchInput);
           setSearchFriends(response.data)
+          setIsSearching(true)
         } else if (searchInput.length < 3) {
           setSearchFriends([])
+          setIsSearching(false)
         }
       } catch (error) {
         console.log(error);
@@ -40,20 +47,13 @@ export function SearchFriends() {
 
   return (
     <>
-      <Input
-        placeholder="voy a ser una busqueda" onChange={(e) => setSearchInput(e.target.value)}
-        gridColumn='span 2'
-      ></Input>
+      <Box m='0.5rem 0.5rem 0rem'>
+        <Input
+          placeholder="Search..." onChange={(e) => setSearchInput(e.target.value)}
+        ></Input>
+      </Box>
       {searchFriends?.map((friend: User) => (
-        <Flex key={nanoid()} alignItems='center' p='2' h='32' border='2px' borderColor='violet'>
-          <Avatar src={friend.imagePath} size='xl' />
-          <Box ml='3'>
-            <Text fontWeight='normal' fontSize='3xl'>
-              {friend.userName}
-            </Text>
-          </Box>
-          <Button onClick={() => sendFriendRequest(friend.id)}>boton 1</Button>
-        </Flex>
+        <FriendProfile friend={friend} isFriendRequest={isFriendRequest} sendFriendRequest={sendFriendRequest}></FriendProfile>
       ))}
     </>
   )
