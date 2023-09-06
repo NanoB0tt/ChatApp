394feb1036cc37607564fc94e60b2a4f3e7ce304
NanoB0tt <marianofassikoropecki@gmail.com>
2023-09-05 21:03:41 -0300

    added user social component

---
+++ src/components/user-social.tsx
@@ -0,0 +1,33 @@
+import { Box, Button, Modal, ModalBody, ModalCloseButton, ModalContent, ModalHeader, ModalOverlay, useDisclosure } from "@chakra-ui/react"
+import { FaUserPlus } from "react-icons/fa";
+import { SearchFriends } from "./search-friends";
+import { FriendRequests } from "./friend-requests";
+import { useState } from "react";
+
+export default function UserSocial() {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const [isSearching, setSearching] = useState(false);
+  return (
+    <>
+      <Button onClick={onOpen} size='md' padding='0' borderRadius='full' marginTop='0.5rem'>
+        <FaUserPlus />
+      </Button>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Add Friends</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Box height='25rem'>
+              <SearchFriends setSearching={setSearching} />
+              {
+                !isSearching && <FriendRequests />
+              }
+            </Box>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+    </>
+  )
+}
