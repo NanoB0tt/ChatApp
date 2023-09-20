b67373372657b1243accf30aab682ed84f830ce3
NanoB0tt <marianofassikoropecki@gmail.com>
2023-09-20 15:18:20 -0300

    error page

---
+++ src/pages/error.tsx
@@ -0,0 +1,27 @@
+import { Flex, Heading, Text } from '@chakra-ui/react';
+import { useRouteError, isRouteErrorResponse } from 'react-router-dom';
+
+export default function ErrorPage() {
+  const error = useRouteError();
+  let errorMessage: string;
+
+  if (isRouteErrorResponse(error)) {
+    // error is type `ErrorResponse`
+    errorMessage = error.error?.message || error.statusText;
+  } else if (error instanceof Error) {
+    errorMessage = error.message;
+  } else if (typeof error === 'string') {
+    errorMessage = error;
+  } else {
+    console.error(error);
+    errorMessage = 'Unknown error';
+  }
+
+  return (
+    <Flex height='100vh' alignItems='center' justifyContent='center' direction='column'>
+      <Heading>Oops!</Heading>
+      <Text>Sorry, an unexpected error has occurred.</Text>
+      <Text>{errorMessage}</Text>
+    </Flex>
+  );
+};
